d1dace7731795e33a70e47cc0bd29133c212ee0c
Samarth Porwal <samarthporwal@myanatomy.in>
2024-11-05 18:31:39 +0530

    Returning html

--- challenges/views.py
+++ challenges/views.py
@@ -23,7 +23,7 @@ monthly_challenges = {
 def monthly_challenges_number(request, month):
     months = list(monthly_challenges.keys())
     if month > len(months):
-        return HttpResponseNotFound("Invalid Month")
+        return HttpResponseNotFound("<h1>Invalid Month<h1>")
     redirect_month = months[month-1]
     redirect_path = reverse("month-challenge", args=[redirect_month])   # /challenge/january
     return HttpResponseRedirect(redirect_path)
@@ -32,6 +32,7 @@ def monthly_challenges_number(request, month):
 def monthly_challenges(request, month):
     try:
         challenge_text = monthly_challenges[month]
-        return HttpResponse(challenge_text)
+        response_data = f"<h1>{challenge_text}<h1>"
+        return HttpResponse(response_data)
     except:
-        return HttpResponseNotFound("This month is not supported")
+        return HttpResponseNotFound("<h1>This month is not supported<h1>")
