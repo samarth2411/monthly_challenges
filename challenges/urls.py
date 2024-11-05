b2aaa6c00cc89dabf7ca89b8ae72d9bbacbc722d
Samarth Porwal <samarthporwal@myanatomy.in>
2024-11-05 13:00:08 +0530

    loading months and their text dynamically

--- challenges/urls.py
+++ challenges/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("january", views.jan),
-    path("february", views.feb)
+    # path("january", views.jan),
+    # path("february", views.feb),
+    path("<month>", views.monthly_challenges)
 ]
--- challenges/views.py
+++ challenges/views.py
@@ -1,12 +1,21 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
 
 # Create your views here.
 
 
-def jan(request):
-    return HttpResponse("Jan works!!")
+# def jan(request):
+#     return HttpResponse("Jan works!!")
 
 
-def feb(request):
-    return HttpResponse("Feb Works!!")
+# def feb(request):
+#     return HttpResponse("Feb Works!!")
+
+def monthly_challenges(request, month):
+    if month == "january":
+        challenge_text = "Jan Text!!"
+    elif month == "february":
+        challenge_text = "Feb Text!!"
+    else :
+        return HttpResponseNotFound("This month not supported!!")
+    return HttpResponse(challenge_text)
